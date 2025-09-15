27d827a9b8adb645e3ae7e8aabaf8757c7ea39e8
ozdemrburak <ozdemrburak@yahoo.com>
2025-09-15 19:26:54 +0300

    update get_prediction.py

--- get_prediction.py
+++ get_prediction.py
@@ -33,12 +33,12 @@ def image_transform(image_path):
 
 
 def predict_image(image_path):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     image = image_transform(image_path)
     model = ResNetRegressor()
     model_path = hf_hub_download(repo_id="theycallmeburki/resnet_regressor_nutrition5k", filename="resnet_regressor_state_dict.pth")
     state_dict = torch.load(model_path, map_location=device)
     model.load_state_dict(state_dict)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     image = image.to(device)
     model = model.to(device)
     with torch.no_grad():
